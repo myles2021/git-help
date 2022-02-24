414e4f1076ceb9eb179dd3b86a171e72235d74b5
myles2021 <86742200+myles2021@users.noreply.github.com>
2022-02-24 16:39:21 +0000

    Create no-compare.sh

---
+++ no-compare.sh
@@ -0,0 +1,6 @@
+# There isn't anything to compare. Nothing to compare, branches are entirely different commit histories
+
+git checkout [BRANCH]   
+git branch main [BRANCH] -f   
+git checkout main   
+git push origin main -f
